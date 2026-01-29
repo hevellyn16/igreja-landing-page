afdcc76b3a515e55fff60e6fba8cf400a950e8bb
hevellyn16 <meirianehev@gmail.com>
2026-01-29 11:34:36 -0300

    feat: improve dashboard financeiro and supabase authentication

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -3,4 +3,11 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'https://lnbwgrdqglxdixpxwuia.supabase.co';
 const supabaseKey = 'sb_publishable_E_XmETCjQn492CPwVnTjJw_HIsJcd-7';
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    storage: sessionStorage, 
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true,
+  },
+});
